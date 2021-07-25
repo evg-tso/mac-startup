5cdf3e4b41e8cad68b7bf8830203b1f776237f11
Yevgeni Tsodikov <evg.tso@gmail.com>
2021-07-25 12:15:25 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -59,3 +59,8 @@ sudo gsed -i '1i auth	   sufficient     pam_tid.so' /etc/pam.d/sudo
 
 # DBeaver
 brew install --cask dbeaver-community
+
+# powerlevel10k
+brew install romkatv/powerlevel10k/powerlevel10k
+echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+
