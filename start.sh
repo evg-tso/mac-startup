481e5e9a2fa27734d8309bb397395ca366f0fcab
Yevgeni Tsodikov <evg.tso@gmail.com>
2021-07-25 11:57:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,3 +19,42 @@ brew install tldr
 brew install htop
 brew install jq
 brew install tree
+
+# Install ZSH
+brew install zsh
+
+# install latest GIT
+brew install git
+brew install git-lfs
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# ZSH autosuggestions, https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# https://formulae.brew.sh/formula/watch, https://www.geeksforgeeks.org/watch-command-in-linux-with-examples/
+which watch || (brew install watch)
+
+# install LastPass
+brew cask install lastpass
+
+# install vscode
+brew cask install visual-studio-code
+
+# Clipy, https://clipy-app.com/
+brew cask install clipy
+
+# Finder: Show hidden files
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# Activity Monitor: Visualize CPU usage in the Activity Monitor Dock icon
+defaults write com.apple.ActivityMonitor IconType -int 5
+
+# Enable TouchID for terminal SUDO
+# https://dev.to/equiman/how-to-use-macos-s-touch-id-on-terminal-5fhg
+# https://unix.stackexchange.com/questions/99350/how-to-insert-text-before-the-first-line-of-a-file
+sudo gsed -i '1i auth	   sufficient     pam_tid.so' /etc/pam.d/sudo
+
+# Brew
+brew install --cask dbeaver-community
