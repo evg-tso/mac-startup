c32d8fcb8b526ce3223d58ebd50e6e33d7eee322
Yevgeni Tsodikov <evg.tso@gmail.com>
2021-07-25 14:15:07 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -64,3 +64,8 @@ brew install --cask dbeaver-community
 brew install romkatv/powerlevel10k/powerlevel10k
 echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
+# Autojump
+brew install autojump
+
+# Rectangle
+brew install --cask rectangle
